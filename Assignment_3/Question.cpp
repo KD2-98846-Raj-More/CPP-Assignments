6d5d5a8d6a932e4575c76acfc01d9dd8dcc5139e
Raj1718 <rajmore1392@gmail.com>
2026-08-29 16:03:19 +0530

    Completed assignment

---
+++ Assignment_3/Question.cpp
@@ -0,0 +1,71 @@
+/*Q1. Write a class to find volume of a Cylinder by using following members. (volume of 
+Cylinder=3.14 * radius * radius *height) ( use static wherever needed. hint-PI) 
+Class having following member functions: 
+Cylinder() 
+Cylinder(double radius, double height) 
+getRadius() 
+setRadius() 
+getHeight() 
+setHeight() 
+calculateVolume() 
+Initialize members using constructor member initializer list*/
+
+#include<iostream>
+using namespace std;
+
+class Cylinder
+{
+    private:
+    double radius;
+    double height;
+   
+     static const double PI ;
+    public:
+     
+    
+    Cylinder(void) : radius(0) , height(0)  
+    {  }
+    Cylinder(double radius , double height ) : radius(radius) , height(height) 
+    {  }
+    void setRadius(double radius)
+    {
+        this->radius = radius;
+    }
+    void setHeight(double height)
+    {
+        this->height = height;
+    }
+    double getRadius(void)
+    {
+        return this->radius;
+    }
+    double getHeight(void)
+    {
+        return this->height;
+    }
+    
+    double calculateVolume()
+    {
+        return PI*radius*radius*height;
+        
+    }
+
+
+    
+};
+
+   const double Cylinder::PI = 3.14;
+
+int main()
+{
+    Cylinder c;
+    c.setRadius(5);
+    c.setHeight(10);
+    c.getRadius();
+    c.getHeight();
+    cout<<"Volume of Cylinder:"<<c.calculateVolume();
+    return 0;
+}
+
+
+
